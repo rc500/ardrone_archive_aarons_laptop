848ff35e8505b375907538b322b8980fa6bdd8f7
Rich Wareham <rjw57@cam.ac.uk>
2011-11-03 23:33:27 +0000

    add some code to parse configuration information

---
+++ ardrone/config.py
@@ -0,0 +1,39 @@
+"""Configuration of the A.R. Drone.
+
+"""
+
+import re
+
+def parse(config):
+	"""Parse configuration strings as returned by the drone.
+
+  >>> import json
+	>>> cfg = '\\n'.join([
+	...		'GENERAL:foo = bar',
+	...		'VIDEO:enable = 1',
+	...		'GENERAL:buzz = quux',
+	...		'VIDEO:be_clever = TRUE',
+	...		'AUDIO:listen = -4',
+	... ])
+	>>> parse(cfg)
+	{'AUDIO': {'listen': '-4'}, 'VIDEO': {'enable': '1', 'be_clever': 'TRUE'}, 'GENERAL': {'foo': 'bar', 'buzz': 'quux'}}
+
+	"""
+	lines = config.splitlines()
+	line_parser = re.compile('^(\w+):(\w+)\s*=\s*(.*)$')
+	rv = { }
+
+	for line in lines:
+		match = line_parser.match(line)
+		if match is None:
+			continue
+		(section, key, value) = match.groups()
+		if section not in rv:
+			rv[section] = { }
+		rv[section][key] = value
+	
+	return rv
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
