b969446fa3ad0fe3f502d0b0663362030076ba6c
Rich Wareham <rjw57@cam.ac.uk>
2011-11-05 18:13:43 +0000

    qtgui: add logging support to main window

--- ardrone/qtgui/app.py
+++ ardrone/qtgui/app.py
@@ -21,7 +21,25 @@ else:
 from ..core.controlloop import ControlLoop
 from ..platform import qt as platform
 
+import logging
+from cgi import escape as html_escape
+
 __resource_dir = os.path.dirname(__file__)
+
+log = logging.getLogger()
+
+class TextEditLogHandler(logging.Handler):
+  def __init__(self, widget):
+    logging.Handler.__init__(self)
+    self._widget = widget
+
+  def emit(self, record):
+    te = self._widget.findChild(QtGui.QTextEdit, 'logTextEdit')
+    if te is None:
+      return
+    msg = html_escape(self.format(record))
+    te.insertHtml('<pre>' + msg + '<br></pre>')
+    te.ensureCursorVisible()
 
 class MainWidget(QtGui.QWidget):
   def __init__(self, *args, **kwargs):
@@ -35,27 +53,41 @@ class MainWidget(QtGui.QWidget):
     self._control = ControlLoop(connection)
     self._control.connect()
 
+    self._log_handler = TextEditLogHandler(self)
+    self._log_handler.setFormatter(logging.Formatter(
+      fmt = '%(asctime)s:%(levelname)s:%(message)s',
+      datefmt = '%Y-%d-%m:%H:%M:%S'
+    ))
+    log.addHandler(self._log_handler)
+
   @qt.Slot()
   def takeOff(self):
+    log.info('Taking off')
     self._control.take_off()
 
   @qt.Slot()
   def land(self):
+    log.info('Landing')
     self._control.land()
 
   @qt.Slot()
   def reset(self):
+    log.info('Reset')
     self._control.reset()
 
   @qt.Slot()
   def hover(self):
+    log.info('Hover')
     self._control.hover()
 
   @qt.Slot()
   def flatTrim(self):
+    log.info('Flat trim')
     self._control.flat_trim()
 
 def main():
+  log.setLevel(logging.DEBUG)
+
   app = QtGui.QApplication(sys.argv)
   ui_file = os.path.join(__resource_dir, 'main_window.ui')
 
