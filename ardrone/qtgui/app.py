c60343763ddc3534e3a983fd54be18387d1cb0a2
Rich Wareham <rjw57@cam.ac.uk>
2011-11-05 13:34:17 +0000

    port code to be PySide friendly

--- ardrone/qtgui/app.py
+++ ardrone/qtgui/app.py
@@ -5,16 +5,27 @@
 import os
 import sys
 
-from PyQt4 import QtCore, QtGui, uic
+try:
+  from ..util import qtcompat as qt
+except ImportError as e:
+  raise ImportError('Qt appears not to be installed: %s' % (str(e),))
+
+QtCore = qt.import_module('QtCore')
+QtGui = qt.import_module('QtGui')
+
+if qt.USES_PYSIDE:
+  from PySide.QtUiTools import QUiLoader
+else:
+  from PyQt4 import uic
 
 from ..core.controlloop import ControlLoop
 from ..platform import qt as platform
 
 __resource_dir = os.path.dirname(__file__)
 
-class MainWindow(QtGui.QMainWindow):
+class MainWidget(QtGui.QWidget):
   def __init__(self, *args, **kwargs):
-    QtGui.QMainWindow.__init__(self, *args, **kwargs)
+    super(MainWidget, self).__init__(*args, **kwargs)
 
     host, port = '192.168.1.1', 5556
     if 'DRONEDEBUG' in os.environ:
@@ -24,23 +35,36 @@ class MainWindow(QtGui.QMainWindow):
     self._control = ControlLoop(connection)
     self._control.connect()
 
+  @qt.Slot()
   def takeOff(self):
     self._control.take_off()
 
+  @qt.Slot()
   def land(self):
     self._control.land()
 
+  @qt.Slot()
   def reset(self):
     self._control.reset()
 
+  @qt.Slot()
   def hover(self):
     self._control.hover()
 
+  @qt.Slot()
   def flatTrim(self):
     self._control.flat_trim()
 
 def main():
-	app = QtGui.QApplication(sys.argv)
-	window = uic.loadUi(os.path.join(__resource_dir, 'main_window.ui'), MainWindow())
-	window.show()
-	sys.exit(app.exec_())
+  app = QtGui.QApplication(sys.argv)
+  ui_file = os.path.join(__resource_dir, 'main_window.ui')
+
+  if qt.USES_PYSIDE:
+    loader = QUiLoader()
+    loader.registerCustomWidget(MainWidget)
+    window = loader.load(ui_file)
+  else:
+    window = uic.loadUi(ui_file)
+  
+  window.show()
+  sys.exit(app.exec_())
