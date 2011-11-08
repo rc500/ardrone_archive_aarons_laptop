c839e9892b46e6944e90896613fddb195a0be8eb
Rich Wareham <rjw57@cam.ac.uk>
2011-11-08 14:38:47 +0000

    add a simple distutils setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='ardrone',
+    version='0.0.1',
+    description='Python interface to the Parrot A.R. Drone',
+    author='Rich Wareham',
+    author_email='rjw57@cantab.net',
+    url='http://example.com/',
+
+    packages=['ardrone', 'ardrone.core', 'ardrone.util', 'ardrone.platform', 'ardrone.qtgui'],
+
+    package_data = {
+      'ardrone.qtgui': ['res/*.ui'],
+    },
+
+    scripts = ['qtgui.py'],
+)
