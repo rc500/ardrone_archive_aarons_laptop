ffed6cf41ed22b917a68f5657644cfee18a176a7
Rich Wareham <rjw57@cam.ac.uk>
2011-11-08 13:50:30 +0000

    navdata: imporove parsing
    Add in the status bitfield constants and start parsing the navdata
    packets.

--- ardrone/core/navdata.py
+++ ardrone/core/navdata.py
@@ -4,6 +4,9 @@
 
 import ctypes as ct
 
+import logging
+log = logging.getLogger()
+
 """The header for a navdata packet."""
 NAVDATA_HEADER = ct.c_int32(0x55667788)
 
@@ -18,6 +21,89 @@ NAVDATA_IPHONE_ANGLES_TAG = ct.c_int16(18)
 
 """The navdata checksum tag for an option packet. (Lifted from navdata.c)"""
 NAVDATA_CKS_TAG = ct.c_int16(0xffff)
+
+# Define masks for ARDrone state
+# 31                                                             0
+#  x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x x -> state
+#  | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+#  | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | FLY MASK : (0) ardrone is landed, (1) ardrone is flying
+#  | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | VIDEO MASK : (0) video disable, (1) video enable
+#  | | | | | | | | | | | | | | | | | | | | | | | | | | | | | VISION MASK : (0) vision disable, (1) vision enable
+#  | | | | | | | | | | | | | | | | | | | | | | | | | | | | CONTROL ALGO : (0) euler angles control, (1) angular speed control
+#  | | | | | | | | | | | | | | | | | | | | | | | | | | | ALTITUDE CONTROL ALGO : (0) altitude control inactive (1) altitude control active
+#  | | | | | | | | | | | | | | | | | | | | | | | | | | USER feedback : Start button state
+#  | | | | | | | | | | | | | | | | | | | | | | | | | Control command ACK : (0) None, (1) one received
+#  | | | | | | | | | | | | | | | | | | | | | | | | Trim command ACK : (0) None, (1) one received
+#  | | | | | | | | | | | | | | | | | | | | | | | Trim running : (0) none, (1) running
+#  | | | | | | | | | | | | | | | | | | | | | | Trim result : (0) failed, (1) succeeded
+#  | | | | | | | | | | | | | | | | | | | | | Navdata demo : (0) All navdata, (1) only navdata demo
+#  | | | | | | | | | | | | | | | | | | | | Navdata bootstrap : (0) options sent in all or demo mode, (1) no navdata options sent
+#  | | | | | | | | | | | | | | | | | | | | Motors status : (0) Ok, (1) Motors Com is down
+#  | | | | | | | | | | | | | | | | | |
+#  | | | | | | | | | | | | | | | | | Bit means that there's an hardware problem with gyrometers
+#  | | | | | | | | | | | | | | | | VBat low : (1) too low, (0) Ok
+#  | | | | | | | | | | | | | | | VBat high (US mad) : (1) too high, (0) Ok
+#  | | | | | | | | | | | | | | Timer elapsed : (1) elapsed, (0) not elapsed
+#  | | | | | | | | | | | | | Power : (0) Ok, (1) not enough to fly
+#  | | | | | | | | | | | | Angles : (0) Ok, (1) out of range
+#  | | | | | | | | | | | Wind : (0) Ok, (1) too much to fly
+#  | | | | | | | | | | Ultrasonic sensor : (0) Ok, (1) deaf
+#  | | | | | | | | | Cutout system detection : (0) Not detected, (1) detected
+#  | | | | | | | | PIC Version number OK : (0) a bad version number, (1) version number is OK
+#  | | | | | | | ATCodec thread ON : (0) thread OFF (1) thread ON
+#  | | | | | | Navdata thread ON : (0) thread OFF (1) thread ON
+#  | | | | | Video thread ON : (0) thread OFF (1) thread ON
+#  | | | | Acquisition thread ON : (0) thread OFF (1) thread ON
+#  | | | CTRL watchdog : (1) delay in control execution (> 5ms), (0) control is well scheduled // Check frequency of control loop
+#  | | ADC Watchdog : (1) delay in uart2 dsr (> 5ms), (0) uart2 is good // Check frequency of uart2 dsr (com with adc)
+#  | Communication Watchdog : (1) com problem, (0) Com is ok // Check if we have an active connection with a client
+#  Emergency landing : (0) no emergency, (1) emergency
+
+ARDRONE_FLY_MASK            = 1 << 0  #/*!< FLY MASK : (0) ardrone is landed, (1) ardrone is flying */
+ARDRONE_VIDEO_MASK          = 1 << 1  #/*!< VIDEO MASK : (0) video disable, (1) video enable */
+ARDRONE_VISION_MASK         = 1 << 2  #/*!< VISION MASK : (0) vision disable, (1) vision enable */
+ARDRONE_CONTROL_MASK        = 1 << 3  #/*!< CONTROL ALGO : (0) euler angles control, (1) angular speed control */
+ARDRONE_ALTITUDE_MASK       = 1 << 4  #/*!< ALTITUDE CONTROL ALGO : (0) altitude control inactive (1) altitude control active */
+ARDRONE_USER_FEEDBACK_START = 1 << 5  #/*!< USER feedback : Start button state */
+ARDRONE_COMMAND_MASK        = 1 << 6  #/*!< Control command ACK : (0) None, (1) one received */
+ARDRONE_FW_FILE_MASK        = 1 << 7  #/* Firmware file is good (1) */
+ARDRONE_FW_VER_MASK         = 1 << 8  #/* Firmware update is newer (1) */
+#//  ARDRONE_FW_UPD_MASK         = 1 << 9  /* Firmware update is ongoing (1) */
+ARDRONE_NAVDATA_DEMO_MASK   = 1 << 10 #/*!< Navdata demo : (0) All navdata, (1) only navdata demo */
+ARDRONE_NAVDATA_BOOTSTRAP   = 1 << 11 #/*!< Navdata bootstrap : (0) options sent in all or demo mode, (1) no navdata options sent */
+ARDRONE_MOTORS_MASK  	      = 1 << 12 #/*!< Motors status : (0) Ok, (1) Motors problem */
+ARDRONE_COM_LOST_MASK       = 1 << 13 #/*!< Communication Lost : (1) com problem, (0) Com is ok */
+ARDRONE_VBAT_LOW            = 1 << 15 #/*!< VBat low : (1) too low, (0) Ok */
+ARDRONE_USER_EL             = 1 << 16 #/*!< User Emergency Landing : (1) User EL is ON, (0) User EL is OFF*/
+ARDRONE_TIMER_ELAPSED       = 1 << 17 #/*!< Timer elapsed : (1) elapsed, (0) not elapsed */
+ARDRONE_ANGLES_OUT_OF_RANGE = 1 << 19 #/*!< Angles : (0) Ok, (1) out of range */
+ARDRONE_ULTRASOUND_MASK     = 1 << 21 #/*!< Ultrasonic sensor : (0) Ok, (1) deaf */
+ARDRONE_CUTOUT_MASK         = 1 << 22 #/*!< Cutout system detection : (0) Not detected, (1) detected */
+ARDRONE_PIC_VERSION_MASK    = 1 << 23 #/*!< PIC Version number OK : (0) a bad version number, (1) version number is OK */
+ARDRONE_ATCODEC_THREAD_ON   = 1 << 24 #/*!< ATCodec thread ON : (0) thread OFF (1) thread ON */
+ARDRONE_NAVDATA_THREAD_ON   = 1 << 25 #/*!< Navdata thread ON : (0) thread OFF (1) thread ON */
+ARDRONE_VIDEO_THREAD_ON     = 1 << 26 #/*!< Video thread ON : (0) thread OFF (1) thread ON */
+ARDRONE_ACQ_THREAD_ON       = 1 << 27 #/*!< Acquisition thread ON : (0) thread OFF (1) thread ON */
+ARDRONE_CTRL_WATCHDOG_MASK  = 1 << 28 #/*!< CTRL watchdog : (1) delay in control execution (> 5ms), (0) control is well scheduled */
+ARDRONE_ADC_WATCHDOG_MASK   = 1 << 29 #/*!< ADC Watchdog : (1) delay in uart2 dsr (> 5ms), (0) uart2 is good */
+ARDRONE_COM_WATCHDOG_MASK   = 1 << 30 #/*!< Communication Watchdog : (1) com problem, (0) Com is ok */
+ARDRONE_EMERGENCY_MASK      = 1 << 31  #/*!< Emergency landing : (0) no emergency, (1) emergency */
+
+def split(data):
+  ndh = NavDataHeader.from_buffer_copy(data[0:ct.sizeof(NavDataHeader)])
+  if not ndh.valid():
+    log.error('Got invalid navdata packet')
+    return (ndh, None)
+
+  got_checksum_option = False
+  next_option_start = ct.sizeof(NavDataHeader)
+
+  while not got_checksum_option and next_option_start < len(data):
+    obh = OptionBlockHeader.from_buffer_copy(
+        data[next_option_start:(next_option_start+ct.sizeof(OptionBlockHeader))])
+    next_option_start += obh.size
+
+  return (ndh, [])
 
 def checksum(data):
   """Compute a checksum for the passed data. data should be a sequence of bytes.
