f91a13ea979ded84c0d8eb508984375dd91db194
Rich Wareham <rjw57@cam.ac.uk>
2011-11-16 19:25:16 +0000

    add links for viewing the source to the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,13 @@ sys.path.insert(0, os.path.abspath('../examples'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.autodoc']
+extensions = [
+  'sphinx.ext.intersphinx',
+  'sphinx.ext.todo',
+  'sphinx.ext.pngmath',
+  'sphinx.ext.autodoc',
+  'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
