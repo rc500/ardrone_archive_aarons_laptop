5c178f04f30c6475abe5c8b95ded4fbf14413918
Rich Wareham <rjw57@cam.ac.uk>
2011-11-30 16:18:00 +0000

    videodata: added a line which makes sure that the right ardrone package is loaded
    On my system, I run the examples as 'python examples/videodata.py' which
    results in the path '..' not pointing to the location of the ardron
    package. Add a line which explicitly finds the path relative to the
    example file.

--- examples/videodata.py
+++ examples/videodata.py
@@ -10,6 +10,7 @@ import socket, time
 # This makes sure the path which python uses to find things when using import
 # can find all our code.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # The magic runes required to import the Qt modules in a platform-independent
 # way
