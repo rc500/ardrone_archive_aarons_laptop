9797412b3fe2c04f715523a48936805d616c5a20
Rich Wareham <rjw57@cam.ac.uk>
2011-12-01 12:57:10 +0000

    libp264: refactor build system to expose unecessary modules
    Comment out some more unecessary code in the build system and add some
    convenience shell scripts for building the native code.

---
+++ compile-native-mingw-win32.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Work out what directory this script is in and hence the build directory
+this_dir=`dirname $0`
+build_dir="${this_dir}/build/native-mingw-win32"
+
+# Move into the build directory (creating it if necessary)
+if [ ! -d "${build_dir}" ]; then
+  mkdir -p "${build_dir}"
+fi
+cd "${build_dir}"
+
+# Run cmake and make the librar{y,ies}.
+cmake -DCMAKE_TOOLCHAIN_FILE=../../mingw-toolchain.cmake ../..
+make all install
+
