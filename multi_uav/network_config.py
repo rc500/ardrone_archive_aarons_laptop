24c0959cdf85583d4d341f27cd66e5d87fdc489e
Aaron Croucher <aaron.croucher@gmail.com>
2012-02-24 12:18:06 +0000

    add bind host for drone 1 and drone 2

--- multi_uav/network_config.py
+++ multi_uav/network_config.py
@@ -1,7 +1,6 @@
-control_host = "127.0.0.1"
-
 drone1 = {
 				'host' : 						"192.168.1.1",	# NB - this is the IP which should be set on the drone
+				'control_host' :				 "127.0.0.1",
 				'at_port' : 						5556,
 				'nav_port' : 						5554,
 				'vid_port' : 						5555,
@@ -11,18 +10,21 @@ drone1 = {
 				'video_data_port' : 				5562,
 				'control_data_listening_port' :		3456,
 				'video_data_listening_port' :		3457,
+				'bind_host':						'192.168.1.2',
 				};
 				
 drone2 = {
 				'host' :						"192.168.2.1",	# NB - this is the IP which should be set on the drone
-				'at_port' :							5656,
-				'nav_port' :						5654,
-				'vid_port' : 						5655,
+				'control_host' :				 "127.0.0.1",
+				'at_port' :							5556,
+				'nav_port' :						5554,
+				'vid_port' : 						5555,
 				'config_port' :						5659,
 				'control_port' :					5660,
 				'control_data_port' :				5661,
 				'video_data_port' :					5662,
 				'control_data_listening_port' :		3556,
 				'video_data_listening_port' :		3557,
+				'bind_host':						'192.168.2.2',
 				};
 
