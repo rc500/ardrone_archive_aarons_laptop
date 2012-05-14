c0af1f9f0d987f3a8cc09092391a75d28b684479
Aaron Croucher <aaron.croucher@gmail.com>
2012-05-14 15:25:13 +0100

    SwarmStates: Improved transition through state system 	- changed checking for exit conditions to allow 'and' and 'or' exit conditions for a state 	- each state has a maintain() and transition(state_id) function to allow more control over state/cooperation of system

--- multi_uav/SwarmStates.py
+++ multi_uav/SwarmStates.py
@@ -45,22 +45,34 @@ class State(object):
 		Check the exit conditions against swarm status.
 		If state requires changing then do so to the correct state.
 		"""
+		# Count for conditions which have been met
+		conditions_met_count = 0
+
 		# Check exit condition for each state against all exit conditions for the respective state
 		for state in self.state_ids:
 			for key in self.exit_conditions[state].keys():
 				#print ("checking condition against: %s" % key)
 				#print ("comparisson: %s" % ([self.exit_conditions[state][key],self._coop.swarm_status[key]]))
 				if self.exit_conditions[state][key] == self._coop.swarm_status[key]:
-					self.next_state(state)
-					return
+					conditions_met_count = conditions_met_count + 1
+
+		# Check met conditions against total conditions, accept or reject exit as specified in state
+		if conditions_met_count == len(self.exit_conditions):
+			self.next_state(state)
+		elif conditions_met_count > 0 and self.exit_conditional == 'or':
+			self.next_state(state)
+		elif conditions_met_count == 0 or self.exit_conditional == 'and':
+			pass
+		else: 
+			print("Unexpected condition grouping - check_exit - SwarmStates")
 
 	def next_state(self,state_id):
 		if state_id == 0:
-			self._coop.change_state(SetupState(self._coop,self.drones,self.drone_controllers))
+			self._coop.change_state((SetupState(self._coop,self.drones,self.drone_controllers),0))
 		elif state_id == 1:
-			self._coop.change_state(TaskBad(self._coop,self.drones,self.drone_controllers))
+			self._coop.change_state((TaskBad(self._coop,self.drones,self.drone_controllers),1))
 		elif state_id == 2:
-			self._coop.change_state(TaskGood(self._coop,self.drones,self.drone_controllers))
+			self._coop.change_state((TaskGood(self._coop,self.drones,self.drone_controllers),2))
 
 class SetupState(State):
 	"""
@@ -88,7 +100,7 @@ class SetupState(State):
 		
 		# Set exit conditions
 		self.exit_conditions = [{}, {'height_stable':True}, {}]
-
+		self.exit_conditional = 'and'
 		print("======In Setup State======")
 
 	def maintain(self):
@@ -127,18 +139,26 @@ class TaskBad(State):
 		State.__init__(self,_coop,drones,drone_controllers)
 		
 		# Set exit conditions
-		self.exit_conditions = [{}, {}, {'airprox':False, 'marker_following':True}]
-
+		self.exit_conditions = [{}, {},{'airprox':False, 'marker_following':True}]
+		self.exit_conditional = 'and'
 		print("======Task not being achieved======")
-		self.action()
+	
+	def maintain(self):
+		self.check_exit()
+		pass # It's not hard to carry on doing something badly!
+
+	def transition(self,state_id):
+		if state_id == 0 or state_id == 1:
+			print("Trying to change from TaskBad state into a SwarmState which isn't sensible. No action taken - SwarmState")
 		
-	def action(self):
-		# Get route for drones
-		new_routes = self._coop._navigator.route(self._coop.swarm_status['position'])
-		print("new routes: %s" %new_routes)
-		self._coop.send_routes(new_routes)
+		if state_id == 2:
+			# Get route for drones
+			new_routes = self._coop._navigator.route(self._coop.swarm_status['position'])
+			print("new routes: %s" %new_routes)
+			self._coop.send_routes(new_routes)
+			# Check success
+			self.check_exit()
 		
- 
 class TaskGood(State):
 	"""
 	ID = 2
@@ -165,10 +185,14 @@ class TaskGood(State):
 		State.__init__(self,_coop,drones,drone_controllers)
 		
 		# Set exit conditions
-		self.exit_conditions = [{}, {'airprox':True, 'following_marker':False}, {}]
-
+		self.exit_conditions = [{}, {'airprox':True,'following_marker':False}, {}]
+		self.exit_conditional = 'or'
 		print("======Task being achieved======")
-	
-	def action(self):
-		# Do nothing
+
+	def maintain(self):
 		pass
+		# nothing yet - will want to control spacings here
+
+	def transition(self,state_id):
+		pass
+		# nothing yet - or ever?
