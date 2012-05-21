94320ec5d9e04ab9f038ec4dd8b7286133335d9a
nefelige <cng22@cam.ac.uk>
2012-05-21 17:54:31 +0100

    Further testing of the OpenCV optical flow function
    Signed-off-by: nefelige <cng22@cam.ac.uk>

--- cng22_tests/feature_detection/optical_flow_test.py
+++ cng22_tests/feature_detection/optical_flow_test.py
@@ -1,4 +1,5 @@
-# Testing the optical flow OpenCV function. 
+# Testing the optical flow OpenCV function,
+#info on how to use from http://nullege.com/codes/search/cv.CalcOpticalFlowBM
 
 from PIL import Image
 import ImageFilter
@@ -7,15 +8,16 @@ import numpy as np
 import sys
 
 #load greyscale image
-img1 = cv.LoadImageM("s1.jpg",cv.CV_LOAD_IMAGE_GRAYSCALE)
+img1 = cv.LoadImageM("t1.jpg",cv.CV_LOAD_IMAGE_GRAYSCALE)
 #load colour image for displaying
-im1 = cv.LoadImageM("s1.jpg");
+im1 = cv.LoadImageM("t1.jpg");
 
 #load greyscale image
-img2 = cv.LoadImageM("s2.jpg",cv.CV_LOAD_IMAGE_GRAYSCALE)
+img2 = cv.LoadImageM("t2.jpg",cv.CV_LOAD_IMAGE_GRAYSCALE)
 #load colour image for displaying
-im2 = cv.LoadImageM("s2.jpg");
+im2 = cv.LoadImageM("t2.jpg");
 
+a=img1
 
 vel_size = (img1.width- 8, img1.height - 8)
 #create storage space for the x and y velocity images
@@ -24,14 +26,19 @@ vely = cv.CreateImage(vel_size, cv.IPL_DEPTH_32F, 1)
 #calculate optical flow
 cv.CalcOpticalFlowBM(img1,img2, (8,8), (1,1), (8,8), 0, velx, vely)
 
-#printing out the values of velx for testing, commented out-
-#code runs too slowly otherwise
-##for i in range(312) :
-##    for j in range(232):
-##        print velx[i,j]
-    
-    
 
+scribble = cv.CreateImage(cv.GetSize(im1), 8, 3)
+print 'ok'
+for y in range(0,228, 4):
+    for x in range(0,308, 4):
+        cv.Line(scribble, (x, y), (int(x+velx[y,x]), int(y + vely[y,x])), (0,255,0))
+        print(x,y)
+cv.Line(a, (640/5,0), (640/5,480), (255,0,0))
+cv.Line(a, (0,360/5), (640,360/5), 255)
+
+
+cv.NamedWindow("test",cv.CV_WINDOW_AUTOSIZE)
+cv.ShowImage("test", scribble)
 
 cv.NamedWindow("test1",cv.CV_WINDOW_AUTOSIZE)
 cv.ShowImage("test1", velx)
